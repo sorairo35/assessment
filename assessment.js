5d6a2ef0b4d64bb4d241cb6966f08624355ce182
sorairo35 <89232637+sorairo35@users.noreply.github.com>
2021-08-20 11:13:02 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -15,50 +15,45 @@ function removeAllChildren(element) {
   }
 }
 
-assessmentButton.onclick = function() {
-    const userName = userNameInput.value;
-    if (userName.length === 0) {
-        // 名前が空の時は処理を終了する
-        return;
-    }
-userNameInput.onkeydown = event => {
-    if (event.key === 'Enter') {
-      assessmentButton.onclick();
-    }
-  };
+assessmentButton.onclick = () => {
+  const userName = userNameInput.value;
+  if (userName.length === 0) {
+    // 名前が空の時は処理を終了する
+    return;
+  }
 
-    //TODO 診断結果表示エリアの作成
-    resultDivided.innerText = " ";
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivided.appendChild(header);
+  // 診断結果表示エリアの作成
+  removeAllChildren(resultDivided);
+  const header = document.createElement('h3');
+  header.innerText = '診断結果';
+  resultDivided.appendChild(header);
 
   const paragraph = document.createElement('p');
   const result = assessment(userName);
   paragraph.innerText = result;
   resultDivided.appendChild(paragraph);
 
-    // TODO ツイートエリアの作成
-    tweetDivided.innerText = "";
-    const anchor = document.createElement('a');
-    const hrefValue = 
-      'https://twitter.com/intent/tweet?button_hashtag=' +
-      encodeURIComponent('あなたのいいところ') +
-      '&ref_src=twsrc%5Etfw';
-anchor.setAttribute('href', hrefValue);
-anchor.className = 'twitter-hashtag-button';
-anchor.setAttribute('data-text', '診断結果の文章');
-anchor.innerText = 'Tweet #あなたのいいところ';
-tweetDivided.appendChild(anchor);
+  // ツイートエリアの作成
+  removeAllChildren(tweetDivided);
+  const anchor = document.createElement('a');
+  const hrefValue =
+    'https://twitter.com/intent/tweet?button_hashtag=' +
+    encodeURIComponent('あなたのいいところ') +
+    '&ref_src=twsrc%5Etfw';
+  anchor.setAttribute('href', hrefValue);
+  anchor.className = 'twitter-hashtag-button';
+  anchor.setAttribute('data-text', result);
+  anchor.innerText = 'Tweet #あなたのいいところ';
+  tweetDivided.appendChild(anchor);
 
-// widgets.js の設定
-const script = document.createElement('script');
-script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
-tweetDivided.appendChild(script);
+  // widgets.js の設定
+  const script = document.createElement('script');
+  script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+  tweetDivided.appendChild(script);
 };
 
 const answers = [
-  '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
+  '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
   '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
   '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
   '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
@@ -83,13 +78,13 @@ const answers = [
  */
 function assessment(userName) {
   // 全文字のコード番号を取得してそれを足し合わせる
-  let sumOfCharCode = 0;
+  let sumOfcharCode = 0;
   for (let i = 0; i < userName.length; i++) {
-    sumOfCharCode = sumOfCharCode + userName.charCodeAt(i);
+    sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
   }
 
   // 文字のコード番号の合計を回答の数で割って添字の数値を求める
-  const index = sumOfCharCode % answers.length;
+  const index = sumOfcharCode % answers.length;
   let result = answers[index];
 
   result = result.replaceAll('{userName}', userName);
